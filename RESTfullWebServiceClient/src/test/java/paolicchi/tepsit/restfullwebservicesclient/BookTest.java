2532169deaaf64a6e2a22f426013f2bf1dd3c142
IlPaoli <paolicchi.davide06@gmail.com>
2025-03-11 18:36:30 +0100

    Finished the test client, it works

---
+++ RESTfullWebServiceClient/src/test/java/paolicchi/tepsit/restfullwebservicesclient/BookTest.java
@@ -0,0 +1,29 @@
+package paolicchi.tepsit.restfullwebservicesclient;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import junit.framework.TestCase;
+
+public class BookTest extends TestCase {
+	public void testGet() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080/api").path("books").path("1");
+		Response response = target.request().get();
+		System.out.println("Response code: " + response.getStatus());
+		Book book = response.readEntity(Book.class);
+		System.out.println("Title: " + book.getTitle());
+	}
+	public void testAdd() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080/api").path("books");
+		Book book = new Book();
+		book.setTitle("Test");
+		book.setLanguage("english");
+		Response response = target.request().post(Entity.entity(book, MediaType.APPLICATION_JSON));
+		System.out.println("Response code: " + response.getStatus());
+	}
+}
